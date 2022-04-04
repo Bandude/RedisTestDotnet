83a360374bd8f927ac2f675d745df4dab99cc560
Craig Jones <craig@designbycraig.com>
2022-04-03 21:50:12 -0500

    Redis Example

---
+++ redis test/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" }
+            });
+
+    var db = redis.GetDatabase();
+    var pong = await db.PingAsync();
+    Console.WriteLine(pong);
+
+Console.WriteLine("Enter a Key: ");
+string? key = Console.ReadLine();
+Console.WriteLine("Enter a value: ");
+string? value = Console.ReadLine();
+
+
+db.StringSet(key, value);
+
+
+while (true)
+{
+    string searchValue = Console.ReadLine();
+
+
+    if(db.StringGet(searchValue).ToString() == null)
+    {
+        Console.WriteLine("No Record Found");
+    }
+    else
+    {
+        Console.WriteLine("You found me!!! >> " + db.StringGet(searchValue).ToString());
+        Console.ReadLine();
+        break;
+    }
+}
